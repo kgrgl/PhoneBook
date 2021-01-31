c245ef44659f8f2827c6a6548a7695dd83e11cc6
Kemal Görgülü <kgorgulu@outlook.com>
2021-01-31 18:25:51 +0300

    Models

---
+++ PhoneBook/Models/Contacts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Models
+{
+    public class Contacts
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public enum ContactTypes
+        {
+            Phone,
+            Mail,
+            Location
+        }
+        [Required]
+        [MaxLength(50, ErrorMessage = "İletişim Verisi 50 karakterden fazla olamaz!")]
+        public string ContactText { get; set; }
+        public Persons Match { get; set; }
+        [Required]
+        public ContactTypes ContactType { get; set; }
+    }
+}
