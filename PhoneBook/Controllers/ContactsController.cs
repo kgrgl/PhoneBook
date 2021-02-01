db89d21ad8504737baac220714ab832893c293a8
Kemal Görgülü <kgorgulu@outlook.com>
2021-02-01 15:34:42 +0300

    Control Recieved Data

--- PhoneBook/Controllers/ContactsController.cs
+++ PhoneBook/Controllers/ContactsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,29 +20,131 @@ namespace PhoneBook.Controllers
         {
             _context = context;
         }
-        [HttpGet]
+        [HttpGet, ActionName("GetContacts")]
         public async Task<IActionResult> Index()
         {
             if (_context.Contacts.Count() == 0)
             {
-                return NotFound("Liste bulunamadı");
+                return NotFound("İletişim Bilgileri Bulunamadı");
             }
             return Ok(await _context.Contacts.ToListAsync());
         }
-        [HttpGet("GetContact")]
+        [HttpGet, ActionName("GetContact")]
         public async Task<IActionResult> Details(int? id)
         {
+            if (id == null)
+            {
+                return NotFound("Lütfen Parametre Gönderin!");
+            }
+
             var contacts = await _context.Contacts
-               .FirstOrDefaultAsync(m => m.ID == id);
+                .FirstOrDefaultAsync(m => m.ID == id);
             if (contacts == null)
             {
-                return NotFound("Kişi Bulunamadı");
+                return NotFound("İletişim Bilgisi Bulunamadı!");
+            }
+
+            return Ok(contacts);
+        }
+
+        [HttpPost("Insert")]
+        public async Task<IActionResult> Create([Bind("ID,ContactText,ContactType")] Contacts contacts)
+        {
+            if (contacts.ContactType == Contacts.ContactTypes.Location)
+            {
+                if (!ValidateLocationValues(contacts.ContactText))
+                    return NotFound("Lütfen Geçerli Bir Lokasyon Giriniz!");
+            }
+            else if (contacts.ContactType == Contacts.ContactTypes.Mail)
+            {
+                if (!new EmailAddressAttribute().IsValid(contacts.ContactText))
+                    return NotFound("Lütfen Geçerli Bir Mail Adresi Giriniz!");
+            }
+            else if (contacts.ContactType == Contacts.ContactTypes.Phone)
+            {
+                if (!new PhoneAttribute().IsValid(contacts.ContactText))
+                    return NotFound("Lütfen Geçerli Bir Telefon Numarası Giriniz!");
+            }
+            _context.Add(contacts);
+            await _context.SaveChangesAsync();
+            return Ok(contacts);
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,ContactText,ContactType")] Contacts contacts)
+        {
+            if (id != contacts.ID)
+            {
+                return NotFound("İletişim Bilgisi Bulunamadı!");
+            }
+
+            try
+            {
+                if (contacts.ContactType == Contacts.ContactTypes.Location)
+                {
+                    if (!ValidateLocationValues(contacts.ContactText))
+                        return NotFound("Lütfen Geçerli Bir Lokasyon Giriniz!");
+                }
+                else if (contacts.ContactType == Contacts.ContactTypes.Mail)
+                {
+                    if (!new EmailAddressAttribute().IsValid(contacts.ContactText))
+                        return NotFound("Lütfen Geçerli Bir Mail Adresi Giriniz!");
+                }
+                else if (contacts.ContactType == Contacts.ContactTypes.Phone)
+                {
+                    if (!new PhoneAttribute().IsValid(contacts.ContactText))
+                        return NotFound("Lütfen Geçerli Bir Telefon Numarası Giriniz!");
+                }
+                _context.Update(contacts);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactsExists(contacts.ID))
+                {
+                    return NotFound("İletişim Bilgisi Bulunamadı!");
+                }
+                else
+                {
+                    throw;
+                }
             }
             return Ok(contacts);
         }
+
+        [HttpPost("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contacts = await _context.Contacts.FindAsync(id);
+            _context.Contacts.Remove(contacts);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         private bool ContactsExists(int id)
         {
             return _context.Contacts.Any(e => e.ID == id);
+        }
+        private bool ValidateLocationValues(string pointValue)
+        {
+            bool pointResult = false;
+            double price;
+            try
+            {
+                string[] values = pointValue.Split(',');
+                if (values.Length > 2)
+                    pointResult = false;
+                else if (!Double.TryParse(values[0], out price))
+                    pointResult = false;
+                else if (!Double.TryParse(values[1], out price))
+                    pointResult = false;
+                return true;
+            }
+            catch (Exception)
+            {
+                pointResult = false;
+            }
+            return pointResult;
         }
     }
 }
--- PhoneBook/Controllers/PersonsController.cs
+++ PhoneBook/Controllers/PersonsController.cs
@@ -24,20 +24,71 @@ namespace PhoneBook.Controllers
         {
             if (_context.Persons.Count() == 0)
             {
-                return NotFound("Liste bulunamadı");
+                return NotFound("Kişi Listesi Bulunamadı");
             }
             return Ok(await _context.Persons.ToListAsync());
         }
         [HttpGet("GetPerson")]
         public async Task<IActionResult> Details(int? id)
         {
+            if (id == null)
+            {
+                return NotFound("Lütfen Parametre Gönderin!");
+            }
+
             var persons = await _context.Persons
                 .FirstOrDefaultAsync(m => m.ID == id);
-            if (persons==null)
+            if (persons == null)
             {
-                return NotFound("Kişi Bulunamadı");
+                return NotFound("Kişi Bilgisi Bulunamadı!");
+            }
+
+            return Ok(persons);
+        }
+
+        [HttpPost("Insert")]
+        public async Task<IActionResult> Create(Persons persons)
+        {
+            _context.Add(persons);
+            await _context.SaveChangesAsync();
+            //return RedirectToAction(nameof(Index));
+            return Ok(persons);
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> Edit(int id, Persons persons)
+        {
+            if (id != persons.ID)
+            {
+                return NotFound("Kişi Bilgisi Bulunamadı!");
+            }
+
+            try
+            {
+                _context.Update(persons);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonsExists(persons.ID))
+                {
+                    return NotFound("Kişi Bilgisi Bulunamadı!");
+                }
+                else
+                {
+                    throw;
+                }
             }
             return Ok(persons);
+        }
+
+        [HttpPost("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var persons = await _context.Persons.FindAsync(id);
+            _context.Persons.Remove(persons);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         private bool PersonsExists(int id)
