1f78b43963f57aeb176edbe6ed208802a3338bb3
Kemal Görgülü <kgorgulu@outlook.com>
2021-02-02 17:02:26 +0300

    Docker file add

--- PhoneBook/obj/Debug/net5.0/PhoneBook.AssemblyInfo.cs
+++ PhoneBook/obj/Debug/net5.0/PhoneBook.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0a1a247e-f961-4057-b314-1e35e7286c7a")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
