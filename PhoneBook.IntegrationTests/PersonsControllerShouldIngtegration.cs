2db58c9f46fc361fa89fb92d9619b61bea650c50
Kemal Görgülü <kgorgulu@outlook.com>
2021-02-04 10:57:09 +0300

    addin unit tests

---
+++ PhoneBook.IntegrationTests/PersonsControllerShouldIngtegration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneBook.IntegrationTests
+{
+    public class PersonsControllerShouldIngtegration
+    {
+        private readonly HttpClient _client;
+
+        public PersonsControllerShouldIngtegration(TestFixture fixture)
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task GetPersonsShouldDoOk()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/Persons");
+            var response = await _client.GetAsync(request.RequestUri);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ PhoneBook.UnitTests/PersonsControllerShould.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Controllers;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneBook.UnitTests
+{
+    public class PersonsControllerShould
+    {
+        [Fact]
+        public async Task CreateNewContact()
+        {
+            var options = new DbContextOptionsBuilder<PhoneDbContext>()
+            .UseInMemoryDatabase(databaseName: "Test_PhoneBook")
+            .Options;
+
+            using (var inMemoryContext = new PhoneDbContext(options))
+            {
+                var service = new PersonsController(inMemoryContext);
+                await service.Create(new Persons { Ad = "Deneme", Soyad = "deneme soyadı",Firma="Test Firması" });
+            }
+            using (var inMemoryContext = new PhoneDbContext(options))
+            {
+                Assert.Equal(1, await inMemoryContext.Persons.CountAsync());
+            }
+        }
+
+    }
+}
